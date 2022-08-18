7838c1ac935d9dd5e6dc20d7ef766e060e23db96
Yu Chen <yc14600@my.bristol.ac.uk>
2022-08-18 23:06:06 +0100

    recover

--- encoder_decoder_to_image.py
+++ encoder_decoder_to_image.py
@@ -8,8 +8,8 @@ import torch
 import os
 from models.generator import MlpVAE as MlpVAE_model
 
-def image_generator(counter, images, testset):
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+def image_generator(counter, images, testset,path,device='cpu'):
+    device = torch.device(device)
     #testset  = MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
     # Create DataLoader objects. These will give us our batches of training and testing data.
     #batch_size = 100
@@ -38,7 +38,7 @@ def image_generator(counter, images, testset):
         test_images = test_images.numpy()
         test_images = np.transpose(test_images, (1, 2, 0))
         plt.imshow(test_images)
-        plt.savefig(os.getcwd() + '/results_images/images_previous/image'+str(counter)+'.jpg')
+        plt.savefig(os.path.join(path,'images_previous','image'+str(counter)+'.jpg'))
 
 
         #reconstructions = reconstructions.view(reconstructions.size(0), 1, 28, 28)
@@ -47,7 +47,6 @@ def image_generator(counter, images, testset):
         reconstructions = reconstructions[:50]
         reconstructions = np.transpose(make_grid(reconstructions, 10, 5).numpy(), (1, 2, 0))
         plt.imshow(reconstructions)
-        plt.savefig(os.getcwd()+'/results_images/images_after/image'+str(counter)+'.jpg')
-
+        plt.savefig(os.path.join(path,'images_after','image'+str(counter)+'.jpg'))
 
 __all__ = ["image_generator"]
